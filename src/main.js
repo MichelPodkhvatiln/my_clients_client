d4bb51127da3b1b3c32133ce872927424757bc9d
MichelPodkhvatilin <podkhvatilin.work@gmail.com>
2021-02-16 21:21:38 +0200

    add bulma from npm

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+require("@/scss/main.scss");
 
 Vue.config.productionTip = false;
 
