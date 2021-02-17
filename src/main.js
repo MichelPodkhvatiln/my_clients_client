1f9bf102697c0e1f550094134ca41cc7d2dc0de0
MichelPodkhvatilin <podkhvatilin.work@gmail.com>
2021-02-17 19:35:54 +0200

    add vuelidate

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import VModal from "vue-js-modal";
+import Vuelidate from "vuelidate";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 require("@/scss/main.scss");
 
 Vue.use(VModal);
+Vue.use(Vuelidate);
 
 Vue.config.productionTip = false;
 
