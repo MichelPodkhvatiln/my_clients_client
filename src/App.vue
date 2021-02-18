8ef1c135cfa32d60cca2a688260f7b2f3e01cb25
MichelPodkhvatilin <podkhvatilin.work@gmail.com>
2021-02-19 00:59:00 +0200

    add app loader

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
   <div id="app">
     <default-layout v-if="!isLoading" />
-    <div v-else class="loading-block">loading</div>
+    <div v-else class="loading-block">
+      <half-circle-spinner
+        :animation-duration="1000"
+        :size="80"
+        color="#00d1b2"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import DefaultLayout from "@/components/layout/DefaultLayout.vue";
 import { mapActions } from "vuex";
+import { HalfCircleSpinner } from "epic-spinners";
+import DefaultLayout from "@/components/layout/DefaultLayout.vue";
 
 export default {
   name: "App",
   components: {
     DefaultLayout,
+    HalfCircleSpinner,
   },
   data() {
     return {
@@ -48,5 +56,13 @@ export default {
 <style lang="scss">
 html {
   overflow: hidden !important;
+}
+
+.loading-block {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
 }
 </style>
