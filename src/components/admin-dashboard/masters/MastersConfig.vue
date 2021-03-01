b5bce195d054c11930ef756b22e6c49efd897bd3
MichelPodkhvatilin <podkhvatilin.work@gmail.com>
2021-03-01 21:49:31 +0200

    add base master config components

--- src/components/admin-dashboard/masters/MastersConfig.vue
+++ src/components/admin-dashboard/masters/MastersConfig.vue
@@ -1,13 +1,84 @@
 <template>
-  <div>
-    Masters config
-  </div>
+  <section class="masters-config">
+    <header class="masters-config__header">
+      <div class="masters-config__header--title">
+        <h1 class="title">
+          Masters management
+        </h1>
+      </div>
+      <div class="masters-config__header--buttons">
+        <button class="button is-link button-with-icon">
+          Add new master
+
+          <plus-icon class="button-icon" />
+        </button>
+      </div>
+    </header>
+
+    <ul class="masters-config__list">
+      <li class="masters-config__list--item">
+        <master-list-item />
+      </li>
+      <li class="masters-config__list--item">
+        <master-list-item />
+      </li>
+      <li class="masters-config__list--item">
+        <master-list-item />
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script>
+import PlusIcon from "@/components/icons/PlusIcon.vue";
+import MasterListItem from "@/components/admin-dashboard/masters/_partial/MasterListItem.vue";
+
 export default {
-  name: "MastersConfig"
+  name: "MastersConfig",
+  components: {
+    PlusIcon,
+    MasterListItem
+  }
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.masters-config {
+  width: 100%;
+  height: 100%;
+}
+
+.masters-config__header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0;
+  margin-bottom: 15px;
+  min-height: 60px;
+}
+
+.masters-config__list {
+  list-style: none;
+}
+
+.masters-config__list--item {
+  &:not(:last-child) {
+    margin-bottom: 10px;
+  }
+}
+
+.button-with-icon {
+  position: relative;
+  padding-right: 30px;
+}
+
+.button-icon {
+  position: absolute;
+  top: 50%;
+  right: 8px;
+  transform: translateY(-50%);
+  width: 16px;
+  height: 16px;
+  fill: #ffffff;
+}
+</style>
---
+++ src/components/admin-dashboard/masters/_partial/MasterListItem.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="box master">
+    <div class="master__avatar--wrapper has-background-primary">
+      <avatar-icon class="master__avatar--icon" />
+    </div>
+
+    <div class="master__content--main">
+      <p class="is-size-5 has-text-weight-semibold mb-2">
+        Name
+      </p>
+      <p class="is-size-5 has-text-info">
+        Salon name
+      </p>
+    </div>
+
+    <div class="master__content--buttons">
+      <div class="buttons">
+        <button class="button is-link">
+          Edit
+        </button>
+        <button class="button is-danger">
+          Remove
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AvatarIcon from "@/components/icons/AvatarIcon.vue";
+
+export default {
+  name: "MasterListItem",
+  components: {
+    AvatarIcon
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.master {
+  position: relative;
+  padding: 10px 15px 10px 80px;
+  min-height: 60px;
+
+  &:hover {
+    .master__content--buttons {
+      opacity: 1;
+    }
+  }
+}
+
+.master__avatar--wrapper {
+  position: absolute;
+  top: 50%;
+  left: 15px;
+  transform: translateY(-50%);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 52px;
+  height: 52px;
+  border-radius: 50%;
+  overflow: hidden;
+}
+
+.master__avatar--icon {
+  width: 40px;
+  height: 40px;
+  fill: #ffffff;
+}
+
+.master__content--main {
+  line-height: 1;
+}
+
+.master__content--buttons {
+  position: absolute;
+  top: 50%;
+  right: 0;
+  transform: translateY(-50%);
+  display: flex;
+  align-items: center;
+  padding: 0 15px;
+  height: 100%;
+  opacity: 0;
+  background-color: #ffffff;
+  transition: opacity 0.15s ease-in;
+}
+</style>
