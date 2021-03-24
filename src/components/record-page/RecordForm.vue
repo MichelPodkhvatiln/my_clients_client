6a8ca24708292053c13b028fd5489fd03a7cf9df
MichelPodkhvatilin <podkhvatilin.work@gmail.com>
2021-03-24 19:15:14 +0200

    add service select

--- src/components/record-page/RecordForm.vue
+++ src/components/record-page/RecordForm.vue
@@ -3,24 +3,46 @@
     <div class="columns pt-4">
       <div v-if="!isLoading" class="column is-6 is-offset-3">
         <h3 class="title is-3 has-text-centered">
-          Selected salon: {{ salonName }}
+          Выбранный салон: {{ salonName }}
         </h3>
 
         <div class="field">
-          <label class="label">Master</label>
+          <label class="label">Мастер</label>
           <div class="select is-fullwidth">
-            <select :disabled="!salonMasters.length">
+            <select :disabled="!salonMasters.length" @change="onMasterSelect">
+              <option value="null">
+                Выберите мастера
+              </option>
               <option
                 v-for="master in salonMasters"
                 :key="master.value"
                 :value="master.value"
-                placeholder="Please, select your master"
               >
                 {{ master.title }}
               </option>
             </select>
           </div>
         </div>
+
+        <div class="field">
+          <label class="label">Услуга</label>
+          <div class="select is-fullwidth">
+            <select :disabled="!formData.master" @change="onServiceSelect">
+              <option value="null">
+                Выберите услугу
+              </option>
+              <option
+                v-for="service in masterServices"
+                :key="service.value"
+                :value="service.value"
+              >
+                {{ service.title }}
+              </option>
+            </select>
+          </div>
+        </div>
+
+        {{ formData }}
       </div>
     </div>
   </div>
@@ -41,7 +63,13 @@ export default {
     return {
       isLoading: false,
       salonInfo: null,
-      mastersList: []
+      servicesInfo: [],
+      mastersList: [],
+      masterInfo: null,
+      formData: {
+        master: null,
+        service: null
+      }
     };
   },
   async beforeMount() {
@@ -54,6 +82,8 @@ export default {
       });
 
       if (!this.salonInfo) return;
+
+      this.servicesInfo = await this.getServicesList(true);
 
       this.mastersList = await this.getSalonMastersList({
         salonId: this.salonId,
@@ -80,11 +110,55 @@ export default {
           value: master.id
         };
       });
+    },
+    masterServices() {
+      if (
+        !this.servicesInfo.length ||
+        !this.mastersList.length ||
+        !this.masterInfo
+      )
+        return [];
+
+      return this.servicesInfo
+        .filter(service => this.masterInfo.services.includes(service._id))
+        .map(service => {
+          return {
+            title: service.name,
+            value: service._id
+          };
+        });
     }
   },
   methods: {
     ...mapActions("salonModule", ["getSalonById"]),
-    ...mapActions("mastersModule", ["getSalonMastersList"])
+    ...mapActions("servicesModule", ["getServicesList"]),
+    ...mapActions("mastersModule", ["getSalonMastersList", "getMasterById"]),
+    async onMasterSelect(evt) {
+      const value = evt.target.value;
+
+      if (value === "null") {
+        this.masterInfo = null;
+        this.formData.master = null;
+        return;
+      }
+
+      try {
+        this.masterInfo = await this.getMasterById(value);
+        this.formData.master = value;
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    onServiceSelect(evt) {
+      const value = evt.target.value;
+
+      if (value === "null") {
+        this.formData.service = null;
+        return;
+      }
+
+      this.formData.service = value;
+    }
   }
 };
 </script>
